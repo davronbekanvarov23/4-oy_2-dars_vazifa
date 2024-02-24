0aa319c1d92f63201eb35741d2afd87b3b02bfe7
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-02-24 14:59:45 +0500

    complated

---
+++ app.js
@@ -0,0 +1,38 @@
+// const yosh = "22"; //string
+// console.log(yosh); //string
+// console.log(Number(yosh));//number
+
+// console.log(yosh+1) //221 (concatenation)
+// console.log(Number(yosh)+1) //23
+
+// const ism='Davronbek'
+// console.log(Number(ism))
+
+// console.log('men '+2002+'chi yilda tavallud topganman')
+
+// console.log("30" - "10" - "5");
+
+// Falsy qiymatar
+
+// console.log(Boolean(false));
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+// console.log(Boolean(0n));
+
+// // truthy qiymatlar
+// console.log(Boolean("davronbek"));
+// console.log(Boolean(22));
+// console.log(Boolean({}));
+// console.log(Boolean([]));
+// console.log(Boolean("0"));
+// console.log(Boolean("false"));
+
+// const ism = "Davronbek";
+// if (ism) {
+//   console.log("sizning ismingiz: " + ism);
+// } else {
+//   console.log("iltimos ism kiriting");
+// }
